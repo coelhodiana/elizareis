a3a0687ebc0a7b1975e34d81f75a8d68ce888251
coelhodiana <coelhodiana04@gmail.com>
2023-09-13 00:08:30 -0400

    feat: form contato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ContatoComponent } from './pages/contato/contato.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  {
+    path: "contato",
+    component: ContatoComponent
+  },
   {
     path: "",
     component: HomeComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
 registerLocaleData(ptBr);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ContatoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
