2a070d9998395939bfc1f5ecd96d1d35452bd419
coelhodiana <coelhodiana04@gmail.com>
2023-09-13 23:28:15 -0400

    feat: project page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ContatoComponent } from './pages/contato/contato.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProjetoComponent } from './pages/projeto/projeto.component';
 
 const routes: Routes = [
   {
     path: "contato",
     component: ContatoComponent
+  },
+  {
+    path: "projeto",
+    component: ProjetoComponent
   },
   {
     path: "",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContatoComponent } from './pages/contato/contato.component';
+import { ProjetoComponent } from './pages/projeto/projeto.component';
 
 registerLocaleData(ptBr);
 
 @NgModule({
-  declarations: [AppComponent, ContatoComponent],
+  declarations: [AppComponent, ContatoComponent, ProjetoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
